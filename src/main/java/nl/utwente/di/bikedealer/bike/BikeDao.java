409d1a7c4739826b08f17160cab680b2ed720a00
jjkramok@gmail.com <Appelsap7>
2015-05-19 16:22:30 +0200

    stuff

--- src/main/java/nl/utwente/di/bikedealer/bike/BikeDao.java
+++ src/main/java/nl/utwente/di/bikedealer/bike/BikeDao.java
@@ -1,6 +1,8 @@
 package nl.utwente.di.bikedealer.bike;
 
 import javax.ws.rs.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by tim on 19-5-15.
@@ -8,4 +10,25 @@ import javax.ws.rs.Path;
 
 @Path("/dao")
 public class BikeDao {
+
+    public Map<Integer, Bike> bikeMap = new HashMap<Integer, Bike>();
+
+
+    private static BikeDao instance;
+
+    private BikeDao() {
+
+    }
+
+    public Map<Integer, Bike> getBikeMap() {
+        return bikeMap;
+    }
+
+    public static BikeDao getInstance() {
+        if (instance == null){
+            instance = new BikeDao();
+        }
+        return instance;
+    }
+
 }
--- src/main/java/nl/utwente/di/bikedealer/bike/BikeService.java
+++ src/main/java/nl/utwente/di/bikedealer/bike/BikeService.java
@@ -1,40 +1,63 @@
 package nl.utwente.di.bikedealer.bike;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by tim on 19-5-15.
  */
 
-@Path("/resources")
+@Path("/resources/rest")
 public class BikeService {
 
     @POST
-    @Produces(MediaType.TEXT_HTML)
     public String createBike(Bike bike) {
         return null;
     }
 
-    public String getBikes() {
+    @GET
+    @Path("/colour={colour}&gender={gender}")
+    @Produces(MediaType.TEXT_HTML)
+    public String getBikes(@PathParam("colour") String colour, @PathParam("gender") String gender) {
+        String first    = "<html> <body> <h1> All bikes according to " + colour + " and " + gender + " </h1>";
+        String last     = " </body> </html>";
+        Map<Integer, Bike> bikeMap = BikeDao.getInstance().getBikeMap();
+        List<Bike> result = new ArrayList<Bike>();
+        for (Bike bike : bikeMap.values()) {
+            if (bike.getColour().equals(colour) && bike.getGender().equals(gender)) {
+                result.add(bike);
+            }
+        }
+        return first + result.toString() + last;
+    }
+
+    @GET
+    @Path("/bikes/{bikeId}")
+    @Produces(MediaType.TEXT_HTML)
+    public String getBikeDetails(@PathParam("bikeId") String bikeId) {
         return null;
     }
 
-    public String getBikeDetails() {
-        return null;
-    }
-
+    @PUT
+    @Path("/bikes/{bikeId}")
+    @Produces(MediaType.TEXT_HTML)
     public String updateBikes(Bike bike) {
         return null;
     }
 
-    public String deleteBike() {
+    @DELETE
+    @Path("/bikes/{bikeId}")
+    public String deleteBike(@PathParam("bikeId") String bikeId) {
         return null;
     }
 
-    public String orderBike() {
+    @POST
+    @Path("/bikes/{bikeId}/order")
+    @Produces(MediaType.TEXT_HTML)
+    public String orderBike(@PathParam("bikeId") String bikeId) {
         return null;
     }
 }
